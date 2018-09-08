538a1682a00e7f099175484a75ec192db2536554
Jamie Feeny <feeny.jamie@gmail.com>
2018-09-08 10:59:28 -0700

    Added new game functionality

--- SnakesLadders/main.m
+++ SnakesLadders/main.m
@@ -13,45 +13,52 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        
+        //---------Setup
+        
         Player *currentPlayer = [[Player alloc] init];
         PlayerManager *coach = [[PlayerManager alloc] init];
         InputHandler *inputHandler = [[InputHandler alloc] init];
         int whosTurn = -1;
+        Boolean newGame = YES;
         
-        
-        //---------Number of players
-        
-        
-        //NSInteger entered = [coach.players count];
-        while ([coach.players count] == 0){
-            NSString *inputStringForNumberofPlayers = [inputHandler userInputForPrompt:@"How many players?:"];
-            int numberOfPlayers = [inputStringForNumberofPlayers intValue];
-            [coach createPlayer:numberOfPlayers];
-            //NSLog(@"\n%@",[coach.players componentsJoinedByString:@" "]);
+        while (newGame == YES){
+            //---------Input number of players
             
-        }
-        
-        while (currentPlayer.youWin == NO) {
-            
-            //---------GamePlay
-            if (whosTurn < coach.players.count-1){
-                whosTurn ++;
-            }else{
-                whosTurn = 0;
+            while ([coach.players count] == 0){
+                NSString *inputStringForNumberofPlayers = [inputHandler userInputForPrompt:@"How many players?:"];
+                int numberOfPlayers = [inputStringForNumberofPlayers intValue];
+                [coach createPlayer:numberOfPlayers];
             }
-            currentPlayer = [coach.players objectAtIndex:whosTurn];
-            //grab the first person from the array
-            NSString *inputStringForRoll = [inputHandler userInputForPrompt:[NSString stringWithFormat: @"%@, please roll the die:", currentPlayer.name]];
-            if ([inputStringForRoll isEqualToString:@"roll"]||[inputStringForRoll isEqualToString:@"r"]){
+            
+            while (currentPlayer.youWin == NO && [coach.players count] != 0){
                 
-                [currentPlayer roll];
-                
-            }else if ([inputStringForRoll isEqualToString:@"quit"]||[inputStringForRoll isEqualToString:@"q"]){
-                [currentPlayer quitGame:whosTurn];
-                
-            }else {
-                NSLog(@"Incorrect input, dumbass!");
-                whosTurn --;
+                //---------GamePlay
+                if (whosTurn < coach.players.count-1){
+                    whosTurn ++;
+                }else{
+                    whosTurn = 0;
+                }
+                currentPlayer = [coach.players objectAtIndex:whosTurn];
+                NSString *inputStringForRoll = [inputHandler userInputForPrompt:[NSString stringWithFormat: @"%@, please roll the die:", currentPlayer.name]];
+                if ([inputStringForRoll isEqualToString:@"roll"]||[inputStringForRoll isEqualToString:@"r"]){
+                    
+                    [currentPlayer roll];
+                    
+                }else if ([inputStringForRoll isEqualToString:@"quit"]||[inputStringForRoll isEqualToString:@"q"]){
+                    [currentPlayer quitGame:whosTurn :coach.players];
+                    whosTurn --;
+                }else {
+                    NSLog(@"Incorrect input, dumbass!");
+                    whosTurn --;
+                }
+            }
+            NSString *inputStringForNewGame = [inputHandler userInputForPrompt:@"Anyone up for a new game? (Y/N):"];
+            if ([inputStringForNewGame isEqualToString:@"y"]) {
+                newGame = YES;
+            }else{
+                newGame = NO;
+                NSLog(@"Alright, be that way.  Hurumph!");
             }
         }
     }
